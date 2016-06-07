bb80d82ab814ffbcb285344ccb6c1f0ec7021bb2
Diego Valle-Jones <diegovalle@gmail.com>
2016-06-07 15:21:49 -0500

    take into account that there can be missing values

--- R/src/heatmap.R
+++ R/src/heatmap.R
@@ -52,7 +52,7 @@ mxc <- get_latest_data()
 heatmap(mxc)
 
 try({
-  if(max(mxc$value) >= 140) {
+  if(max(mxc$value, na.rm = TRUE) >= 140) {
     max_idx <- which(mxc$value == max(mxc$value))
     SENDGRID_PASS <- Sys.getenv("SENDGRID_PASS") 
     SENDGRID_USER <- Sys.getenv("SENDGRID_USER")

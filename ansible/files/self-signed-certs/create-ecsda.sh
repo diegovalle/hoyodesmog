82fbbc85678e356a75ffe57e7c8883a657e05c91
diego <diegovalle@gmail.com>
2016-08-24 18:00:16 -0500

    keys

--- ansible/files/self-signed-certs/create-ecsda.sh
+++ ansible/files/self-signed-certs/create-ecsda.sh
@@ -1,5 +1,9 @@
 domain=hoyodesmog.diegovalle.net
 
-openssl ecparam -genkey -name > account.key
+openssl genrsa  2048 > account.key
 openssl ecparam -genkey -name prime256v1 -noout -out domain.key
-openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:$domain,DNS:www.$domain")) > cert.csr
+
+#for a single domain
+openssl req -new -sha256 -key domain.key -subj "/CN="$domain"" > cert.csr
+
+#openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:$domain,DNS:www.$domain")) > cert.csr

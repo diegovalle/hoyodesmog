10c32c3ba806a7d735f5c1a9b3f0d6b1ad3be9d3
diego <diegovalle@gmail.com>
2016-08-24 20:12:10 -0500

    self-signed certs

--- ansible/files/self-signed-certs/create-ecsda.sh
+++ ansible/files/self-signed-certs/create-ecsda.sh
@@ -7,3 +7,6 @@ openssl ecparam -genkey -name prime256v1 -noout -out domain.key
 openssl req -new -sha256 -key domain.key -subj "/CN="$domain"" > cert.csr
 
 #openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:$domain,DNS:www.$domain")) > cert.csr
+
+openssl x509 -req -sha256 -days 365 -in cert.csr -signkey domain.key -out cert.crt
+openssl x509 -in cert.crt -out chained.pem -outform PEM

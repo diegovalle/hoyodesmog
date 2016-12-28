5c431deb87e7720a4b4960f86d9227ca285b5cea
diego <diegovalle@gmail.com>
2016-12-27 20:51:05 -0600

    moving average

--- web/js/small-multiples.js
+++ web/js/small-multiples.js
@@ -1,3 +1,39 @@
+function movingAvg(array, count, qualifier) {
+
+    // calculate average for subarray
+    var avg = function(array, qualifier) {
+
+        var sum = 0, count = 0, val;
+        for (var i in array) {
+            val = array[i];
+            if (val !== null) {
+                sum += val;
+                count++;
+            }
+        }
+
+        return sum / count;
+    };
+
+    var result = [], val;
+
+    // pad beginning of result with null values
+    for (var i = 0; i < count - 1; i++)
+        result.push(null);
+
+    // calculate average for each subarray and add to result
+    for (var i = 0, len = array.length - count; i <= len; i++) {
+
+        val = avg(array.slice(i, i + count), qualifier);
+        if (isNaN(val))
+            result.push(null);
+        else
+            result.push(val);
+    }
+
+    return result;
+}
+
 $(document).ready(function() {
     d3.json(stations_file, function(data) {
 
@@ -23,12 +59,27 @@ $(document).ready(function() {
                   '\"></div></div>')
                     .appendTo('#small-multiples');
 
+                var extra_ma = false;
+                if (typeof(include_ma) !== 'undefined')
+                    if (include_ma === true) {
+                        var pollution_values = _.map(data[i],
+                                                     function(x) {
+                                                         return x.value;
+                                                     });
+                        var ma = [];
+                        movingAvg(pollution_values, period).forEach(function(d, j) {
+                            ma.push({'date': data[i][j].date,
+                                     'value': isFinite(d) ? d : null});
+                        });
+                        extra_ma = true;
+                    }
+
                 MG.data_graphic({
                     title: data[i][0].station_code,
                     description: data[i][0].station_name,
                     show_tooltips: true,
                     show_secondary_x_label: false,
-                    data: data[i],
+                    data: extra_ma ? [data[i], ma] : data[i],
                     //y_extended_ticks: true,
                     yax_count: 3,
                     xax_count: 3,

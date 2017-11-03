f1659919fb25bd31eee5544d234b94d024f1482e
diego <diegovalle@gmail.com>
2017-11-02 21:58:48 -0600

    use idw360 from the aire.zmvm package

--- R/src/wdr_heatmap.R
+++ R/src/wdr_heatmap.R
@@ -16,64 +16,15 @@ df$datetime <- strptime(str_c(df$date, " ", df$hour),
 # Convert to MXC time
 df$datetime_mxc <- as.POSIXct(format(df$datetime, tz="America/Mexico_City", usetz=TRUE))
 
-# modified to work with degrees from phylin::idw
-idw360 <-
-  function(values, coords, grid, method="Shepard", p=2, R=2, N=15) {
-   
-    d.real <- real.dist(coords, grid)
-    dimensions <- dim(d.real)
-    
-    methods <- c("Shepard", "Modified", "Neighbours")
-    method <- agrep(method, methods)
-    
-    if (method == 1) {
-      w <- 1/d.real**p
-    } else if (method == 2) {
-      w <- ((R-d.real) / (R*d.real))**p
-    } else if (method == 3) {
-      calcneighbours <- function(x, N) {
-        x[order(x)][N:length(x)] <- Inf
-        return(x)
-      }
-      newdist <- t(apply(d.real, 1, calcneighbours, N))
-      w <- 1/newdist**p
-    }
-    
-    # To allow the idw to act on points with same coordinate, rows are checked
-    # for infinite weights. When found, points with Inf are 1 and all others 
-    # have 0 weight
-    for (i in 1:nrow(w)) {
-      if (sum(is.infinite(w[i,])) > 0){
-        w[i,!is.infinite(w[i,])] <- 0
-        w[i,is.infinite(w[i,])] <- 1
-      }
-    }
-    #browser()
-    
-    y <- (sin(values*(pi/180)))
-    # Interpolation
-    w.sum <- apply(w, 1, sum, na.rm=TRUE)
-    wy <- w %*% diag(y)
-    uy <- apply(wy/w.sum, 1, sum, na.rm=TRUE)
-    
-    x <- (cos(values*(pi/180)))
-    # Interpolation
-    w.sum <- apply(w, 1, sum, na.rm=TRUE)
-    wx <- w %*% diag(x)
-    ux <- apply(wx/w.sum, 1, sum, na.rm=TRUE)
-    
-    res <- atan2(uy, ux) * (180 / pi)
-    res <- ifelse(res < 0, 360 + res, res)
-    
-    data.frame(Z = res)
-  }
-
-
-heatmap_wdr <- function(){
+heatmap_wdr <- function(df){
+  # coordinates of the wind measuring stations
   geog.o3 <- df[,c("lat", "lon", "value")]
   coordinates(geog.o3) <- ~lon+lat
+  is.projected(geog.o3)
+  proj4string(geog.o3) <- CRS("+proj=longlat +ellps=WGS84 +no_defs +towgs84=0,0,0")
   #spplot(geog.o3)
   
+  # create a 100x100 grid based on the stations
   pixels = 100
   geog.grd <- expand.grid(x=seq((min(coordinates(geog.o3)[,1])-.1),
                                 (max(coordinates(geog.o3)[,1])+.1),
@@ -84,25 +35,18 @@ heatmap_wdr <- function(){
   
   grd.pts <- SpatialPixels(SpatialPoints((geog.grd)))
   grd.pts <- as(grd.pts, "SpatialGrid")
+  proj4string(grd.pts) <- CRS("+proj=longlat +ellps=WGS84 +no_defs +towgs84=0,0,0")
   
   #plot(grd.pts, cex = 1.5, col = "grey")
   #points(geog.o3, pch = 1, col = "red", cex = 1)
   
-  idwc <- idw360(df$value, df[,c("lon", "lat")], as.data.frame(grd.pts))
-  #real.dist(as.matrix(df[,c("lon", "lat")]), as.matrix(as.data.frame(grd.pts)))
-  geog.idw <- idw(value ~ 1, geog.o3, grd.pts, debug.level =0)
-  
-  #spplot(geog.idw["var1.pred"])
-  temp <- as.data.frame(geog.idw)
-  temp <- cbind(idwc, NA, temp[,c("x", "y")])
-  names(temp)=c("var1.pred", "var1.var", "lon", "lat")
-  coordinates(temp) <- ~lon+lat
+  # Inverse distance weighting
+  idw <- idw360(geog.o3$value, geog.o3, grd.pts)
+  idw <- cbind(idw, NA, as.data.frame(grd.pts))
+  names(idw)=c("var1.pred", "var1.var", "lon", "lat") 
+  #coordinates(temp) <- ~lon+lat
   #spplot(temp["var1.pred"])
-  #atan2( ((sin(340*(pi/180))+ sin(320*(pi/180)) ) / 3), 
-  #       ((cos(340*(pi/180))+ cos(320*(pi/180)) ) / 3) ) * 180 / pi
-  
-  idw = as.data.frame(temp)
-  names(idw) <- c("var1.pred", "var1.var", "lon", "lat")
+
   
   write_json("output/wdr_data.json",
              idw[,c("var1.pred", "lon", "lat")], "values")
@@ -110,5 +54,5 @@ heatmap_wdr <- function(){
              df)
 }
 
-heatmap_wdr()
+heatmap_wdr(df)
 

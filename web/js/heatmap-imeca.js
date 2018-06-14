db9e2026f0d04302837cbf52cdade4768a095c92
diego <diegovalle@gmail.com>
2018-06-14 15:44:01 -0500

    change tile server url so as not to get an https error

--- web/js/heatmap-imeca.js
+++ web/js/heatmap-imeca.js
@@ -53,7 +53,7 @@ var gridx, gridy;
 
 //var points = data; // data loaded from data.js
 var leafletMap = L.map('map');
- L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+ L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
      maxZoom: 19
  }).addTo(leafletMap);

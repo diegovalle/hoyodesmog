7d1d3a82dca1e43f35cf55ba93408afc613db463
diego <git@diegovalle.net>
2024-02-10 14:40:49 -0600

    Remove ansible directory

--- ansible/README.md
+++
@@ -1,6 +0,0 @@
-Ansible for hoyodesmog.diegovalle.net
-===============================================
-
-- Modify the hosts file with the ip of the server
-- run renew_cert.sh as letsencrypt user to obtain the tls certificates
-  after the dns is updated
--- ansible/roles/nginx/files/self-signed-certs/create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout domain-selfsigned.key -out cert-selfsigned.crt -subj "/C=US/ST=PA/L=Collegeville/O=hoyodesmog.diegovalle.net/OU=/CN=hoyodesmog.diegovalle.net"
-wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
-cat cert-selfsigned.crt  intermediate.pem > chained.pem
-
